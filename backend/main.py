1561e962381eb86c87d4527b950b511b8afe1c53
divyasri-mummadi <divyamummadi007@gmail.com>
2026-08-21 16:12:42 +0530

    Add sensor data API

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class SensorData(BaseModel):
+    device_id: str
+    zone: str
+    pressure: float
+    flow: float
+    acoustic: float
+    ph: float
+    tds: float
+    turbidity: float
+
 
 @app.get("/")
 def home():
     return {"message": "HydroIQ Backend Running"}
+
+
+@app.get("/sensors/latest")
+def latest_sensors():
+    return {
+        "device_id": "ESP32_01",
+        "zone": "Zone_A",
+        "pressure": 2.6,
+        "flow": 118,
+        "acoustic": 0.82,
+        "ph": 7.2,
+        "tds": 312,
+        "turbidity": 4.8
+    }
+
+
+@app.post("/sensors")
+def receive_sensor_data(data: SensorData):
+    return {
+        "message": "Sensor data received",
+        "data": data
+    }
